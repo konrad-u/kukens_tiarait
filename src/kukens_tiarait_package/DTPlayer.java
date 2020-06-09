b4743ba19574690b4451fc7eeddd4b7e5c5e2c2e
konrad-u <konradaukens@gmail.com>
2020-06-09 18:55:23 +0200

    Server initiates, and there is a main class, but no game start yet

---
+++ src/kukens_tiarait_package/DTPlayer.java
@@ -0,0 +1,36 @@
+package kukens_tiarait_package;
+
+import lenz.htw.tiarait.ColorChange;
+import lenz.htw.tiarait.net.NetworkClient;
+
+import java.io.IOException;
+
+public class DTPlayer {
+
+    public static void main(String [] args) throws IOException {
+
+        NetworkClient networkClient = new NetworkClient(null, "...");
+
+        int player = networkClient.getMyPlayerNumber(); // 0-3 (ACHTUNG! andere Nummerierung als beim ColorChange)
+
+        while (networkClient.isAlive()) {
+            int botNr = 0;
+            // steuerung
+            float x = networkClient.getX(player, botNr);
+            float y = networkClient.getY(player, botNr);
+
+            networkClient.isWall(7, 11); //true wenn bei Koordinate 7,11 ein Hindernis steht
+
+            networkClient.setMoveDirection(0, 0.1f, -0.8f);
+            networkClient.setMoveDirection(1, 0.1f, 1.8f);
+            networkClient.setMoveDirection(2, -5.1f, -0.8f);
+
+            ColorChange cc;
+            while ((cc = networkClient.getNextColorChange()) != null) {
+                // cc in eigene Struktur einarbeiten
+                //z.B.brett[cc.x][cc.y] = cc.newColor;
+                //cc.newColor; //0 = leer, 1-4 = spieler
+            }
+        }
+    }
+}
